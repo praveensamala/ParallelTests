eb9b351a73f3b9adc304ac8d7c4eb570a2ea6938
Praveen <Praveen@Laptop-Dell7559>
2019-09-02 13:29:16 +0530

    updated code

--- src/main/java/com/training/multi/base/BaseTest.java
+++ src/main/java/com/training/multi/base/BaseTest.java
@@ -2,6 +2,7 @@ package com.training.multi.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -12,11 +13,15 @@ public class BaseTest {
 		if (browser.equalsIgnoreCase("chrome")) {
 			if (os.equalsIgnoreCase("windows")) {
 				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//chromedriver.exe");
+				driver = new ChromeDriver();
 			}
 			else {
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--no-sandbox");
 				System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver_linux");
+				driver = new ChromeDriver(options);
 			}
-			driver = new ChromeDriver();
+			
 		} else if (browser.equalsIgnoreCase("firefox")) {
 			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//geckodriver.exe");
 			driver = new FirefoxDriver();
